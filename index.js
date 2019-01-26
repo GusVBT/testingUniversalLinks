76a7a740d9a7814212a2fa266336eb86aae18902
Unknown <gus.vbt@gmail.com>
2019-01-26 14:15:22 -0200

    /

--- index.js
+++ index.js
@@ -3,5 +3,14 @@ const express = require('express')
 const app = express()
 const http = require('http').Server(app)
 
+
+app.get('/', (request, response, next) => {
+    response.status(200).json('Universal Link server')
+})
+
+app.use((err, request, response, next) => {
+    response.status(400).json(err)
+})
+
 http.listen(2000)
 console.log('listen in port =', 2000)
